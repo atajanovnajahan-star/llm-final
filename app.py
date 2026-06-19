61f4bada028a685c17054e939cd13eb18c7b87a5
atajanovnajahan-star <atajanovnajahan@gmail.com>
2026-06-20 05:25:20 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -575,21 +575,21 @@ def workflow():
     return render_template("workflow.html")
 
 
+with app.app_context():
+    db.create_all()
+
+    if not User.query.filter_by(username="demo").first():
+        demo = User(
+            username="demo",
+            email="demo@example.com",
+            password=generate_password_hash("123456")
+        )
+        db.session.add(demo)
+        db.session.commit()
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-        if not User.query.filter_by(username="demo").first():
-            demo = User(
-                username="demo",
-                email="demo@example.com",
-                password=generate_password_hash("123456")
-            )
-            db.session.add(demo)
-            db.session.commit()
-
     app.run(debug=True)
-
 
 
 
